f4b9c7d2375d1aecec27414aed4bd643094d7242
sehoon123 <sehoon0519@naver.com>
2024-09-24 01:04:57 +0900

    기본 알람 2개

--- alarm_share/lib/main.dart
+++ alarm_share/lib/main.dart
@@ -13,8 +13,34 @@ Future<void> main() async {
 
   await Alarm.init();
 
+  // 기본 알람 설정
+  await setInitialAlarms();
+
   // Run the app
   runApp(const MyApp());
+}
+
+Future<void> setInitialAlarms() async {
+  final alarms = await Alarm.getAlarms();
+  if (alarms.isEmpty) {
+    final now = DateTime.now();
+    final alarm1 = AlarmSettings(
+      id: 1,
+      dateTime: now.add(Duration(hours: 1)),
+      assetAudioPath: 'assets/sounds/marimba.mp3',
+      notificationTitle: '첫 번째 알람',
+      notificationBody: '첫 번째 알람이 울립니다.',
+    );
+    final alarm2 = AlarmSettings(
+      id: 2,
+      dateTime: now.add(Duration(hours: 2)),
+      assetAudioPath: 'assets/sounds/marimba.mp3',
+      notificationTitle: '두 번째 알람',
+      notificationBody: '두 번째 알람이 울립니다.',
+    );
+    await Alarm.set(alarmSettings: alarm1);
+    await Alarm.set(alarmSettings: alarm2);
+  }
 }
 
 class MyApp extends StatelessWidget {
